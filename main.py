541f4eb39dbac03ac027b61e88802469c2eeb7dc
tok3rat0r <owenmjones1@gmail.com>
2022-10-05 14:30:55 +1100

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+from package2 import module2
+
+if __name__ == '__main__':
+    print(module2.sum_squares(3, 4))
---
+++ package2/module2.py
@@ -0,0 +1,7 @@
+from package1 import module1
+
+
+def sum_squares(num1, num2):
+	sq1 = num1 * num1
+	sq2 = num2 * num2
+	return module1.add_nums(sq1, sq2)
